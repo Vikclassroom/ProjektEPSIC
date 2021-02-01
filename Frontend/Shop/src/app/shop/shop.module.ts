3499a2aa95125f7756bc075003ef7ef7e3ae645e
Max <mmaj.classroom@gmail.com>
2021-02-01 18:26:54 +0100

    wejjjjjjj'

--- Frontend/Shop/src/app/shop/shop.module.ts
+++ Frontend/Shop/src/app/shop/shop.module.ts
@@ -4,18 +4,15 @@ import {ShopComponent} from './shop.component';
 import {ProductItemComponent} from './product-item/product-item.component';
 import {SharedModule} from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import {RouterModule} from '@angular/router';
+import {ShopRoutingModule} from './shop-routing.module';
 
 
 @NgModule({
   declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
-  exports: [
-    ShopComponent
-  ],
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule
+    ShopRoutingModule
   ]
 })
 export class ShopModule {
